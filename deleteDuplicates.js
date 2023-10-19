c278372ba22235fed7071fc11f6322898cee8838
unknown <arvid.lindstrom>
2023-10-19 15:06:27 +0200

    Added function to delete duplicates in array

---
+++ deleteDuplicates.js
@@ -0,0 +1,6 @@
+function unique(arr) {
+  //for every array item, run indexOf on the original array searching for 
+  //item starting from current index + 1. If it doesn't exist anywhere else return true.
+  //Gets only the LAST occurence of any value in arr.
+  return arr.filter( (item, index) => arr.indexOf(item, index + 1) == -1);
+}
