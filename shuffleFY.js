2522bd3517fc40d3bd421f95969384c1f60125b2
JohnArvid <arvid-lindstrom@live.se>
2023-10-20 09:36:40 +0200

    Commenting shuffleFY.js

--- shuffleFY.js
+++ shuffleFY.js
@@ -3,8 +3,6 @@ function shuffleFY(array) {
     let j = Math.floor(Math.random() * (i + 1)); // random index from 0 to i
 
     // swap elements array[i] and array[j]
-    // we use "destructuring assignment" syntax to achieve that
-    // you'll find more details about that syntax in later chapters
     // same can be written as:
     // let t = array[i]; array[i] = array[j]; array[j] = t
     [array[i], array[j]] = [array[j], array[i]];
