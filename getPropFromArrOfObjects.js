6de83d4ad7aa76872d5e268cc19668e06c18c1ec
JohnArvid <arvid-lindstrom@live.se>
2023-10-20 08:42:52 +0200

    Comments added to getPropFromArrOfObjects

--- getPropFromArrOfObjects.js
+++ getPropFromArrOfObjects.js
@@ -1,3 +1,7 @@
+//First parameter is an array of objects - arr
+//Second parameter is a property as string - prop
+//Returns array of the property values of prop for each object in arr
+
 function getPropFromArrOfObjects(arr, prop) {
     return arr.map( item => item[prop] );
 }
