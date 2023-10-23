eed7a483aaaec4ea1881c520db030ef378601471
Arvid Lindström <arvid-lindstrom@live.se>
2023-10-23 08:45:03 +0200

    Added comment to maxLen4.js
    Comment to reflect limitations

--- maxLen4.js
+++ maxLen4.js
@@ -1,4 +1,6 @@
 //med klassen maxLen4 på öppna frågor begränsas input till fyra tecken
+//funkar för tillfället enbart på ett item per sida
+
 (function () {
   document.querySelector(".maxLen4 input").setAttribute("maxlength", "4");
 })();
