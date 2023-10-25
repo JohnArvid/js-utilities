b3c8539e2f493cffc300bb24dca8687ff32634e2
JohnArvid <arvid-lindstrom@live.se>
2023-10-25 09:41:10 +0200

    Refactored maxlen4
    Now works with multiple inputs on same page

--- maxLen4.js
+++ maxLen4.js
@@ -1,6 +1,12 @@
-//med klassen maxLen4 på öppna frågor begränsas input till fyra tecken
-//funkar för tillfället enbart på ett item per sida
+//Add css-class mexLen4 on inputs to set attribute maxlength to 4
+//Works for multiple items on same page
+//IIFE to be added in footer and doesn't have to be called explicitly 
 
 (function () {
-  document.querySelector(".maxLen4 input").setAttribute("maxlength", "4");
+  const nodelist = document.querySelectorAll(".maxLen4 input");
+  for (let node of nodelist) { 
+    node.setAttribute("maxlength","4");
+  };
 })();
+
+
