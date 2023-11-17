5ecb7d1a5d8537f0798f6511e664ec7522668a8d
Arvid Lindström <arvidlindstrom@Arvids-MacBook-Air.local>
2023-11-17 11:36:33 +0100

    left-pad because fun

---
+++ leftPad.js
@@ -0,0 +1,10 @@
+function leftPad(str, len, ch) {
+	str = String(str);
+	let i = -1;
+	if (!ch && ch !== 0) ch = ' ';
+	len = len -str.length;
+	while (++i < len) {
+		str = ch + str;
+	}
+	return str;
+}
