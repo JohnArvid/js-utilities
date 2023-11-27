262c86fd9905937b710195f1d05fa92b821030f8
Arvid Lindström <arvid-lindstrom@live.se>
2023-11-27 13:25:40 +0100

    added example of linked list and reverseprint function

--- printLinkedList.js
+++ printLinkedList.js
@@ -4,3 +4,26 @@ function printLinkedList(list) {
         printList(list.next);
     }
 }
+
+function printListReverse(list) {
+    if (list.next) {
+        printListReverse(list.next);
+    }
+
+    console.log(list.value);
+}
+
+
+let list = {
+  value: 1,
+  next: {
+    value: 2,
+    next: {
+      value: 3,
+      next: {
+        value: 4,
+        next: null
+      }
+    }
+  }
+};
