59b45189c51888519225506929079c264d04be17
Arvid Lindström <arvid-lindstrom@live.se>
2023-12-08 15:28:05 +0100

    commented on what it actually returns

--- groupByPoperty.js
+++ groupByPoperty.js
@@ -1,8 +1,9 @@
-//First argument is an array of objects
-//Second argument is a property as string that is unique for every object in array
+// First argument is an array of objects
+// Second argument is a property as string that is unique for every object in array
 function groupByProperty(arr, prop){
     return arr.reduce( (obj, item) => {
         obj[item[prop]] = item;
         return obj;
     }, {});
 }
+// Returns an object with the value of prop as key and each object associated as value
