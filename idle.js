afd15c836f371c3d0f3395ccb1575c1b82ef6f90
Arvid Lindström <arvid-lindstrom@live.se>
2023-12-21 10:37:41 +0100

    updated idle function w click

--- idle.js
+++ idle.js
@@ -1,5 +1,17 @@
 const intervalID = setInterval(myCallback, 500*60);
+const button = document.createElement('button');
+
+document.body.appendChild(button);
+document.body.lastChild.addEventListener('click', clickHandler());
+
+
+
 
 function myCallback() {
+  document.body.lastChild.click();
+}
+
+
+function clickHandler() {
   console.log("idle");
 }
