e7e08e5d730774fb456b98f82ff60f202d2348a7
JohnArvid <arvid-lindstrom@live.se>
2023-12-22 09:09:33 +0100

    deleted idle.html and prep to rewrite idle.js

--- idle.js
+++ idle.js
@@ -12,3 +12,16 @@ function clickHandler() {
 
 
 
+
+//rewrite with wakelock
+console.log("Loaded at " + new Date(Date.now()));
+const intervalID = setInterval(myCallback, 1000 * 60);
+const root = document.getElementById('root');
+root.lastChild.addEventListener('click', clickHandler);
+
+function myCallback() {
+  root.lastChild.click();
+}
+function clickHandler() {
+  location.reload();
+}
