2c21c44b10f01c538a93ba34a3e85a71b88f09b9
JohnArvid <arvid-lindstrom@live.se>
2023-12-22 08:34:48 +0100

    added idle.html

--- idle.js
+++ idle.js
@@ -1,17 +1,14 @@
-const intervalID = setInterval(myCallback, 500*60);
+const intervalID = setInterval(myCallback, 1000 * 60);
 const button = document.createElement('button');
-
 document.body.appendChild(button);
-document.body.lastChild.addEventListener('click', clickHandler());
-
-
-
+document.body.lastChild.addEventListener('click', clickHandler);
 
 function myCallback() {
   document.body.lastChild.click();
 }
-
-
 function clickHandler() {
   console.log("idle");
 }
+
+
+
